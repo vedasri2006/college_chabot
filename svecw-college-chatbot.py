ff9965f211eef106d1910f1c012d2347b9de5e84
vedasri2006 <24b05a4507@svecw.edu.in>
2025-02-28 11:17:23 +0530

    Update svecw-college-chatbot.py

--- svecw-college-chatbot.py
+++ svecw-college-chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import google.generativeai as genai
-from sklearn.metrics.pairwise impor cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 st.set_page_config(page_title="Svecw College Chatbot",layout="centered")
 if "messages" not in st.session_state:
